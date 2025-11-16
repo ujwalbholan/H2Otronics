b6672c5a7104f604c802723655674fb52c4b3fda
Ujwal Bholan <ujwalbholan@gmail.com>
2025-11-16 11:37:37 +0545

    app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,11 +14,11 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import ProtectedRoute from "./components/ProtectedRoute";
 import PublicRoute from "./components/PublicRoute";
-import Dashboard from "./pages/Dashboard/Dashboard.jsx";
-import Alerts from "./pages/Dashboard/Alerts.jsx";
-import Analytics from "./pages/Dashboard/Analytics.jsx";
-import Reports from "./pages/Dashboard/Reports.jsx";
-import Settings from "./pages/Dashboard/Settings.jsx";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Alerts from "./pages/Dashboard/Alerts";
+import Analytics from "./pages/Dashboard/Analytics";
+import Reports from "./pages/Dashboard/Reports";
+import Settings from "./pages/Dashboard/Settings";
 
 import { useState, useEffect } from "react";
 import Cookies from "js-cookie";
